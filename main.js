f801a17c962d1663871a1ffe9880ec20e56cf52d
Mohamed Irshath <106222625+Irsha01@users.noreply.github.com>
2024-05-21 18:02:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow () {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false,
+    }
+  });
+
+  // Load index.html into the new BrowserWindow
+  mainWindow.loadFile('index.html');
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+app.whenReady().then(createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
